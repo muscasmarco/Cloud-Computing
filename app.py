a97bbac3856bf77feb3fd40dd0a8bb71fb7cbcd8
Marco Muscas <muscas.marco@gmail.com>
2020-10-12 20:18:17 +0200

    Updated for test on docker

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def testing_homepage():
     return "Your container is working"
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", debug=True)
+    app.run(host = "127.0.0.1", port = 5000, debug=True)
 
 
 
