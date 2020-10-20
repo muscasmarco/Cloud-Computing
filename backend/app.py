7a8a32a109e426fc64bf3ce387d899600ad1d035
Marco Muscas <muscas.marco@gmail.com>
2020-10-20 23:08:31 +0200

    Fixed port and host

--- backend/app.py
+++ backend/app.py
@@ -430,5 +430,5 @@ def testing_homepage():
 
 
 if __name__ == '__main__':
-    # app.run(host = "0.0.0.0", port = 80, debug = False)
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host = "0.0.0.0", port = 80, debug = False)
+    #app.run(host="127.0.0.1", port=5000, debug=True)
